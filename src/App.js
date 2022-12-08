823de04b5aa1b539ad8b1ebd2933ee48d85bd35d
natalie w <nmwpenguin@gmail.com>
2022-12-08 09:59:14 -0800

    working edits - V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+//* const img = new Image("./Desktop/googleCalendar");
+//* import { SignIn } from "./Pages";
+//* import 'googleCalendar'; from './screenshots'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <title>Your Patient Care</title>
       </header>
+      <div class="titleHead">
+        <p>Welcome to your Centralized Care Hub</p>
+      </div>
+      
+      {/* <div class="login">
+        <p class="loginTitle">Log-In</p>
+        <label class="loginInput">Username: <input type="input" minlength="8" placeholder="LastFirst" required/></label>
+        <label class="loginInput">Password: <input type="password" minlength="8" placeholder="something secure..." required/></label>
+        <input type="submit" class="submission"/>
+        </div>
+  */}
+      <div class="dash-main">
+        <p class="dashTitle">Dashboard</p>
+        <div class="container1">
+          <div class="calendar">
+            <h3>Upcoming Appointments</h3>
+            <a href="https://calendar.google.com/" class="sitelink">
+              <img src="./Users/nataliewallis/Desktop/googleCalendar.png" alt="calendar"/>
+            </a>
+          </div>
+          <div class="medSchedule">
+            <h3>Medication Schedule</h3>
+          </div>
+        </div>
+        <div class="container2">
+          <div class="notes">
+            <h3>Recent Doctors' Notes</h3>
+          </div>
+          <div class="form">
+            <h3>Symptom Change Form</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
